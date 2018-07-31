332f798764e6108b72c04bdff99cff751a5f3baf
JelenaMiljkovic <jelena.miljkovic@t-systems.com>
2018-07-31 10:58:48 +0200

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { QuartzJob } from './quartz-jobs.component';
+import { QuartzJobCliComponent } from './quartz-job-cli/quartz-job-cli.component';
+import { QuartzService } from './quartz.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuartzJob,
+    QuartzJobCliComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [QuartzService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
